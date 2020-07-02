d2647984605b47e3db4e473905f2f8c78bddf67f
Tomas <cunn.tomas@gmail.com>
2020-07-02 11:35:02 -0300

    fix deployment

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from "./app";
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
